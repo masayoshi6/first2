b76c17bc4a8af8735cffab07ef2cfe5396afd5e2
masayoshi6 <okumurakiyoshikou@MacBook-Air.local>
2025-05-18 12:30:33 +0900

    かきくけこ追加

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello and welcome!");
     System.out.println("あいうえお");
+    System.out.println("かきくけこ");
     for (int i = 1; i <= 5; i++) {
       System.out.println("i = " + i);
     }
